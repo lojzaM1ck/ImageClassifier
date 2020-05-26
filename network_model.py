9926cbf8142d5e6f29f91e3653fbff2e247c5a74
lojzaM1ck <65910222+lojzaM1ck@users.noreply.github.com>
2020-05-26 18:16:16 +0200

    Update network_model.py

--- network_model.py
+++ network_model.py
@@ -21,7 +21,6 @@ def preproces(data_dir):
     data_dir = data_dir
     train_dir = data_dir + '/train'
     valid_dir = data_dir + '/valid'
-    test_dir = data_dir + '/test'
 
     data_transforms = {'train': transforms.Compose([transforms.RandomRotation(30),
                                                     transforms.RandomResizedCrop(224),
@@ -34,23 +33,15 @@ def preproces(data_dir):
                                                     transforms.CenterCrop(224),
                                                     transforms.ToTensor(),
                                                     transforms.Normalize([0.485, 0.456, 0.406],
-                                                                         [0.229, 0.224, 0.225])]),
-
-                       'test': transforms.Compose([transforms.Resize(256),
-                                                   transforms.CenterCrop(224),
-                                                   transforms.ToTensor(),
-                                                   transforms.Normalize([0.485, 0.456, 0.406],
-                                                                        [0.229, 0.224, 0.225])])}
+                                                                         [0.229, 0.224, 0.225])])}
 
     # TODO: Load the datasets with ImageFolder
     image_datasets = {'train_data': datasets.ImageFolder(train_dir, transform=data_transforms['train']),
-                      'valid_data': datasets.ImageFolder(valid_dir, transform=data_transforms['valid']),
-                      'test_data': datasets.ImageFolder(test_dir, transform=data_transforms['test'])}
+                      'valid_data': datasets.ImageFolder(valid_dir, transform=data_transforms['valid'])}
 
     # TODO: Using the image datasets and the trainforms, define the dataloaders
     dataloaders = {'trainloader': torch.utils.data.DataLoader(image_datasets['train_data'], batch_size=64, shuffle=True),
-                   'validloader': torch.utils.data.DataLoader(image_datasets['valid_data'], batch_size=64),
-                   'testloader': torch.utils.data.DataLoader(image_datasets['test_data'], batch_size=64)}
+                   'validloader': torch.utils.data.DataLoader(image_datasets['valid_data'], batch_size=64)}
     
     return dataloaders, image_datasets
 
@@ -157,30 +148,18 @@ def load_checkpoint(filepath):
 def preprocess_image(image):
     
     im = Image.open(image)
-    width, height = im.size
+    
+    transform = transforms.Compose([transforms.Resize(256),
+                                    transforms.CenterCrop(224),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize([0.485, 0.456, 0.406], 
+                                                         [0.229, 0.224, 0.225])]) 
 
-    if width > height:
-        ratio = width/height
-        im.thumbnail((ratio*256,256))
-    elif height > width:
-        im.thumbnail((256,height/width*256))
-        
-   
-    new_width, new_height = im.size
-
-    left = (new_width - 224)/2
-    top  = (new_height - 224)/2
-    right = (new_width + 224)/2
-    bottom = (new_height+ 224)/2
-    im=im.crop((left, top, right, bottom))
-    np_image=np.array(im)
-    np_image = np_image / 255
-    means=np.array([0.485, 0.456, 0.406])
-    std= np.array([0.229, 0.224, 0.225])
-    np_image=(np_image-means)/std
-    np_image = np_image.transpose((2,0,1))
-
-    return torch.tensor(np_image)
+    im_transformed = transform(im)
+    array_im = np.array(im_transformed)
+    np_image = torch.from_numpy(array_im).type(torch.FloatTensor)
+    
+    return np_image
 
 def predict(image_path, model, gpu, topk=5):
     
@@ -193,7 +172,6 @@ def predict(image_path, model, gpu, topk=5):
     img = preprocess_image(image_path)
     img = img.to(device)
     img = img.unsqueeze(0)  
-    model = model.double()
     if gpu == True:
         with torch.no_grad():
             model = model.cuda()
